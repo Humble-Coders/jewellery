2948ebdf623ef497c5fb906cd2771f970599afe0
sharnyagoel19 <sgoel_be23@thapar.edu>
2026-01-17 19:24:37 +0530

    Initial Commit

---
+++ jewellery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jewellery
+//
+//  Created by Sharnya  Goel on 17/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jewellery/jewelleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  jewelleryApp.swift
+//  jewellery
+//
+//  Created by Sharnya  Goel on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct jewelleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
